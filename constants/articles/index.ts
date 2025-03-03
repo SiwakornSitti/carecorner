635296591dc5e5eb445d51ab1dd9ae7e15801775
promin04 <prommin.fai@snocko-tech.com>
2025-03-03 09:08:11 +0700

    update acticle

--- constants/articles/index.ts
+++ constants/articles/index.ts
@@ -1,5 +1,7 @@
 import { DATA_17_01_2025 } from "./DATA_17_01_2025";
 import { DATA_10_01_2025 } from "./DATA_10_01_2025";
 import { DATA_16_03_2024 } from "./DATA_16_03_2024";
+import { DATA_23_08_2024 } from "./DATA_23_08_2024";
+import { DATA_27_08_2024 } from "./DATA_27_08_2024";
 
-export const ARTICLES = [ DATA_17_01_2025, DATA_10_01_2025, DATA_16_03_2024];
+export const ARTICLES = [ DATA_17_01_2025, DATA_10_01_2025, DATA_27_08_2024, DATA_23_08_2024, DATA_16_03_2024,];
